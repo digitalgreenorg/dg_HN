9c7d38db611fdb0c795a522b31cc4f0e626436d9
sainivikas <vikassaini480@gmail.com>
2016-12-22 15:49:43 +0530

    changed email body and code cleanup

--- activities/management/commands/monthly_data_reporting.py
+++ activities/management/commands/monthly_data_reporting.py
@@ -10,20 +10,39 @@ class Command(BaseCommand):
 
     def send_mail(self,reporting_file_path):
         today_date = datetime.datetime.now().strftime("%Y-%m-%d")
-        subject = "Health & Nutrition (West Africa) Monthly Data Till %s"%today_date
+        subject = "Health & Nutrition (West Africa): Data received till %s"%(today_date)
         from_email = dg.settings.EMAIL_HOST_USER
-        to_email = ['system@digitalgreen.org', 'avinash@digitalgreen.org', 'vikas@digitalgreen.org', 'vivek@digitalgreen.org']
-        body = "Dear Team,\n\n" + "This is a Monthly automated email to monitor Health & Nutrition (West Africa) data entry.\nThis mail is to keep you updated on the progress in data entry." + "\n\nPlease contact system@digitalgreen.org for any clarification.\n\nThank you.\n"
+        to_email = ['system@digitalgreen.org', 'avinash@digitalgreen.org', 'vikas@digitalgreen.org', 'vivek@digitalgreen.org', 'abhishekchandran@digitalgreen.org']
+        body = "Dear Team,\n\nPlease find the attached Health & Nutrition (West Africa) data entered till %s.\nPlease contact system@digitalgreen.org for any question or clarification.\n\nThank you."%(today_date)
         msg = EmailMultiAlternatives(subject, body, from_email, to_email)
         msg.attach_file(reporting_file_path, 'text/csv' )
         msg.send()
 
+    def check_dict(self,data,year,month,country,state,district,block,village,video):
+        if year not in data:
+            data[year] = dict()
+        if month not in data[year]:
+            data[year][month] = dict()
+        if country not in data[year][month]:
+            data[year][month][country] = dict()
+        if state not in data[year][month][country]:
+            data[year][month][country][state] = dict()
+        if district not in data[year][month][country][state]:
+            data[year][month][country][state][district] = dict()
+        if block not in data[year][month][country][state][district]:
+            data[year][month][country][state][district][block] = dict()
+        if village not in data[year][month][country][state][district][block]:
+            data[year][month][country][state][district][block][village] = dict()
+        if video not in data[year][month][country][state][district][block][village]:
+            data[year][month][country][state][district][block][village][video] = dict()
+
     def handle(self, *args, **options):
         data = dict()
         data_list = []
-        reporting_file_path = 'documents/health_data.csv'
+        reporting_file_path = 'files/health_data.csv'
         months_dic = dict(January=1, February=2, March=3, April=4, May=5, June=6, July=7,
               August=8, September=9, October=10, November=11, December=12)
+
         all_screening = Screening.objects.filter(date__isnull=False)
         for screening in all_screening:
             year = str(screening.date.strftime('%Y'))
@@ -33,25 +52,9 @@ class Command(BaseCommand):
             district = str(screening.village.block.district.district_name.encode('utf-8'))
             block = str(screening.village.block.block_name.encode('utf-8'))
             village = str(screening.village.village_name.encode('utf-8'))
-            videos = screening.videoes_screened.all()
-            
+            videos = screening.videoes_screened.all()    
             for video in videos:
-                if year not in data:
-                    data[year] = dict()
-                if month not in data[year]:
-                    data[year][month] = dict()
-                if country not in data[year][month]:
-                    data[year][month][country] = dict()
-                if state not in data[year][month][country]:
-                    data[year][month][country][state] = dict()
-                if district not in data[year][month][country][state]:
-                    data[year][month][country][state][district] = dict()
-                if block not in data[year][month][country][state][district]:
-                    data[year][month][country][state][district][block] = dict()
-                if village not in data[year][month][country][state][district][block]:
-                    data[year][month][country][state][district][block][village] = dict()
-                if video not in data[year][month][country][state][district][block][village]:
-                    data[year][month][country][state][district][block][village][video] = dict()
+                self.check_dict(data,year,month,country,state,district,block,village,video)
                 if 'TOTAL_SCREENING' not in data[year][month][country][state][district][block][village][video]:
                     data[year][month][country][state][district][block][village][video]['TOTAL_SCREENING'] = 0
                 if 'UNIQUE_MEMBER' not in data[year][month][country][state][district][block][village][video]:
@@ -68,7 +71,6 @@ class Command(BaseCommand):
                     data[year][month][country][state][district][block][village][video]['OTHER_FEMALE'] = set()
                 if 'MALE' not in data[year][month][country][state][district][block][village][video]:
                     data[year][month][country][state][district][block][village][video]['MALE'] = set()
-
                 data[year][month][country][state][district][block][village][video]['TOTAL_SCREENING'] += 1
                 for farmer in screening.farmers_attendance.all():
                     data[year][month][country][state][district][block][village][video]['UNIQUE_MEMBER'].add(farmer.id)
@@ -90,8 +92,8 @@ class Command(BaseCommand):
                     data[year][month][country][state][district][block][village][video]['OTHER_FEMALE'].add(mother)
                 for m in male:
                     data[year][month][country][state][district][block][village][video]['MALE'].add(m)
-
         del all_screening
+
         all_adoption = PersonAdoptPractice.objects.filter(date_of_verification__isnull=False)
         for adoption in all_adoption:
             year = str(adoption.date_of_verification.strftime('%Y'))
@@ -102,23 +104,7 @@ class Command(BaseCommand):
             block = str(adoption.person.village.block.block_name.encode('utf-8'))
             village = str(adoption.person.village.village_name.encode('utf-8'))
             video = adoption.video
-
-            if year not in data:
-                data[year] = dict()
-            if month not in data[year]:
-                data[year][month] = dict()
-            if country not in data[year][month]:
-                data[year][month][country] = dict()
-            if state not in data[year][month][country]:
-                data[year][month][country][state] = dict()
-            if district not in data[year][month][country][state]:
-                data[year][month][country][state][district] = dict()
-            if block not in data[year][month][country][state][district]:
-                data[year][month][country][state][district][block] = dict()
-            if village not in data[year][month][country][state][district][block]:
-                data[year][month][country][state][district][block][village] = dict()
-            if video not in data[year][month][country][state][district][block][village]:
-                data[year][month][country][state][district][block][village][video] = dict()
+            self.check_dict(data,year,month,country,state,district,block,village,video)
             if 'UNIQUE_ADOPTION' not in data[year][month][country][state][district][block][village][video]:
                 data[year][month][country][state][district][block][village][video]['UNIQUE_ADOPTION'] = set()
             if 'UNIQUE_PROMOT' not in data[year][month][country][state][district][block][village][video]:
@@ -131,8 +117,8 @@ class Command(BaseCommand):
                 data[year][month][country][state][district][block][village][video]['UNIQUE_PROMOT'].add(adoption.person_id)
             if adoption.n_one == 1 and adoption.n_two == 1 and adoption.n_three == 1 and adoption.n_four == 1 and adoption.n_five == 1:
                 data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO'].add(adoption.person_id)
-
         del all_adoption
+
         for year in data:
             for month in data[year]:
                 for country in data[year][month]:
@@ -184,8 +170,7 @@ class Command(BaseCommand):
                                         if 'RECALL_ALL_NEGO' not in data[year][month][country][state][district][block][village][video]:
                                             data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO'] = 0
                                         else:
-                                            data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO'] = len(data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO'])
-                                            
+                                            data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO'] = len(data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO'])    
                                         video_title = str(video.title.encode('utf-8'))
                                         total_screening = data[year][month][country][state][district][block][village][video]['TOTAL_SCREENING']
                                         unique_member= data[year][month][country][state][district][block][village][video]['UNIQUE_MEMBER'] 
@@ -199,7 +184,6 @@ class Command(BaseCommand):
                                         unique_promt = data[year][month][country][state][district][block][village][video]['UNIQUE_PROMOT']
                                         recall_all_nego = data[year][month][country][state][district][block][village][video]['RECALL_ALL_NEGO']
                                         data_list.append({'YEAR':year,'MONTH':month,'COUNTRY':country,'REGION':state,'DEPARTMENT':district,'COMMUNE':block,'VILLAGE':village,'VIDEO':video_title,'TOTAL SCREENING':total_screening,'UNIQUE_GROUP':unique_group,'UNIQUE_MEMBER':unique_member,'Pregnant Woman':preg_women,'Mother of child < 5 years old':mother_child,'Adolescent girls (10-19 years old)':adole_girl,'Other females':other_female,'Males':male,'UNIQUE MEMBER REPORT ADOPT PRACTICE':unique_adoption,'UNIQUE MEMBER REPORT PROMOT PRACTICE':unique_promt,'UNIQUE MEMBER REPORT RECALL ALL NON-NEGOTIABLES':recall_all_nego})
-
         del data
         data_list = sorted(data_list,key=lambda ele: (ele['YEAR'],months_dic[ele['MONTH']]),reverse=True)
         headers = ['YEAR','MONTH', 'COUNTRY', 'REGION', 'DEPARTMENT', 'COMMUNE', 'VILLAGE', 'VIDEO', 'TOTAL SCREENING', 'UNIQUE_GROUP', 'UNIQUE_MEMBER', 'Pregnant Woman', 'Mother of child < 5 years old', 'Adolescent girls (10-19 years old)', 'Other females', 'Males', 'UNIQUE MEMBER REPORT ADOPT PRACTICE', 'UNIQUE MEMBER REPORT PROMOT PRACTICE', 'UNIQUE MEMBER REPORT RECALL ALL NON-NEGOTIABLES']
@@ -207,5 +191,4 @@ class Command(BaseCommand):
             dict_writer = csv.DictWriter(final_output,headers)
             dict_writer.writeheader()
             dict_writer.writerows(data_list)
-
         self.send_mail(reporting_file_path)
